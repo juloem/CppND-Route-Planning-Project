1091881fc43a74f3a43a4f73207e4b236ee2c1eb
Julian O <55801542+juloem@users.noreply.github.com>
2024-04-29 11:15:31 +0200

    feat: undo changes in googletest

--- test/utest_rp_a_star_search.cpp
+++ test/utest_rp_a_star_search.cpp
@@ -66,7 +66,6 @@ TEST_F(RoutePlannerTest, TestCalculateHValue) {
     EXPECT_FLOAT_EQ(route_planner.CalculateHValue(start_node), 1.1329799);
     EXPECT_FLOAT_EQ(route_planner.CalculateHValue(end_node), 0.0f);
     EXPECT_FLOAT_EQ(route_planner.CalculateHValue(mid_node), 0.58903033);
-    EXPECT_EQ(1,2);
 }
 
 
